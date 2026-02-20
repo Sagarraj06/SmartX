02b2680fd77a461241dfb0c815d93d470f48eead
Sagar Raj <sagarraj@example.com>
2026-02-20 15:00:40 +0530

    Remove new server/ and client/ folders

--- server/config/googleai.js
+++
@@ -1,3 +0,0 @@
-const { GoogleGenAI } =  require("@google/genai");
-const ai = new GoogleGenAI({apiKey:process.env.google_api_key});
-module.exports = ai;
--- server/config/openai.js
+++
@@ -1,3 +0,0 @@
-const openai = require("openai");
-const client = new openai.OpenAI({apiKey:process.env.openAi_key3});
-module.exports = client;
